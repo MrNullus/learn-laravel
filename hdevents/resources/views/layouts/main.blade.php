e688f25d2e6431b1f774ac05d2b46415459e43a5
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-12-04 17:19:50 -0300

    fix: resolvin problem from tag link

--- hdevents/resources/views/layouts/main.blade.php
+++ hdevents/resources/views/layouts/main.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
     <!-- CSS do Bootstrap -->
-    link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"/>
 
     <!--  CSS da Aplicação -->
     <link rel="stylesheet" href="/css/styles.css"/>
