dc2a6d2ca0945d2e1935720aa3d13db09b979cfd
Etec <70487766+MrNullus@users.noreply.github.com>
2022-12-08 18:18:31 -0300

    docs: add 11 migrations :smille:

--- hdevents/app/Http/Controllers/EventController.php
+++ hdevents/app/Http/Controllers/EventController.php
@@ -4,25 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Event;
+
 class EventController extends Controller
 {
 
   public function index() {
-    $nome = 'Gustavo';
-    $idade = 17;
-    $arr = [1, 2, 3, 4, 5];
-    $nomes = ['Maria', 'Pádme', 'Cycrux', 'Lucanda Barbosa'];
+    $events = Event::all();
 
-    return view(
-      'welcome',
-      [
-        'nome'      => $nome,
-        'idade'     => $idade,
-        'profissao' => 'Programador',
-        'arr'       => $arr,
-        'nomes'     => $nomes
-      ]
-    );
+    return view('welcome', []);
   }
 
   public function create() {
--- hdevents/database/migrations/2022_12_08_210404_create_events_table.php
+++ hdevents/database/migrations/2022_12_08_210404_create_events_table.php
@@ -13,12 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->integer('qty');
-            $table->text('description');
             $table->timestamps();
+            $table->string("title");
+            $table->text("description");
+            $table->string("city");
+            $table->boolean("private");
         });
     }
 
@@ -29,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product');
+        Schema::dropIfExists('events');
     }
 };
